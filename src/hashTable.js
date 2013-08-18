0df1cd96dccad048f03a4df0622c3ef0a861601e
Tyler McGinnis <tylermcginnis33@gmail.com>
2013-08-18 15:47:04 -0700

    add full hash table function

--- src/hashTable.js
+++ src/hashTable.js
@@ -1,31 +1,39 @@
 var HashTable = function(){
   this._limit = 8;
-
-  // Use a limited array to store inserted elements.
-  // It'll keep you from using too much space. Usage:
-  //
-  //   limitedArray.set(3, 'hi');
-  //   limitedArray.get(3); // alerts 'hi'
-  //
-  // There's also a '.each' method that you might find
-  // handy once you're working on resizing
   this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(i);
-  this._storage.set(i, v);
+  var pairs = this._storage.get(i) || [];
+  for(var i = 0; i < pairs.length; i++){
+    if(pairs[i][0] === k){ //If the key already exists, just replace the value
+      pairs[i][1] = v;
+      return
+    }
+  }
+  pairs.push([k,v]);
+  this._storage.set(i, pairs);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  var pairs = this._storage.get(i) || [];
+  for(var i = 0; i < pairs.length; i++){
+    if(pairs[i][0] === k){
+      return pairs[i][1];
+    }
+  }
 };
 
-HashTable.prototype.remove = function(){
+HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var pairs = this._storage.get(i) || [];
+  for(var i=0; i < pairs.length; i++){
+    if(pairs[i][0] === k){
+      var result = pairs.slice(i,1);
+      return result[1];
+    }
+  }
 };
 
-// NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
-// Start by loading those files up and playing with the functions it provides.
-// You don't need to understand how they work, only their interface is important to you
